9c25a9262e0db145ba486b9e509e753fb408d375
Ryunosuke Todate <todateryunosuke@ryunosukes-MacBook-Pro.local>
2021-03-09 06:17:57 +0900

    SwiftUIを使った開発の実験用repository

---
+++ NewMyProject/NewMyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewMyProjectApp.swift
+//  NewMyProject
+//
+//  Created by Ryunosuke Todate on 2021/03/09.
+//
+
+import SwiftUI
+
+@main
+struct NewMyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
